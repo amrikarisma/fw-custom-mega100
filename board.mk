d52ed4c60b9bb53244579795f0bbd871490f3d34
Amri Karisma <amrikarisma@live.com>
2025-02-21 00:04:03 +0700

    try disable lua

--- board.mk
+++ board.mk
@@ -20,9 +20,18 @@ DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::I15
 DDEFS += -DWITH_LUA_CONSUMPTION=FALSE
 DDEFS += -DWITH_LUA_PID=FALSE
 DDEFS += -DWITH_LUA_STOP_ENGINE=FALSE
+DDEFS += -DEFI_LUA=FALSE
 
 
 # This board uses ChibiOS MFS driver on external SPI flash
 include $(PROJECT_DIR)/hw_layer/ports/stm32/use_higher_level_flash_api.mk
 #Serial flash driver
 include $(PROJECT_DIR)/hw_layer/drivers/flash/w25q/w25q_single_spi.mk
+
+# Otherwise writeToFlashNow() is called from ISR context (slow timer callback)
+DDEFS += -DEFI_FLASH_WRITE_THREAD=TRUE
+DDEFS += -DEFI_STORAGE_MFS_EXTERNAL=TRUE
+# Move persistentState out of CCM as it should be accessible by DMA
+DDEFS += -DPERSISTENT_LOCATION=""
+
+DDEFS += -DMIN_FLASH_SIZE=512
