d9e942e94b7a03eb91863afcce363db310033bdc
Amri Karisma <amrikarisma@live.com>
2025-02-22 06:48:53 +0000

    setup docker

--- board.mk
+++ board.mk
@@ -7,25 +7,16 @@ BOARDINC += $(BOARD_DIR)/generated/controllers/generated
 
 # defines SHORT_BOARD_NAME
 include $(BOARD_DIR)/meta-info.env
-
-
-# debug
 DEBUG_LEVEL_OPT = -O0 -ggdb -g
 
 # reduce memory usage monitoring
 DDEFS += -DRAM_UNUSED_SIZE=100
 
 # assign critical LED to a non-existent pin
-DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::B7
+DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::BB7
 
-# This board has 512K STM32F407
-DDEFS += -DMIN_FLASH_SIZE=512
-DDEFS += -DFLASH_ADDR=0x08075000
-# DDEFS += -DEFI_STORAGE_INT_FLASH=FALSE
 
-DDEFS += -DEFI_LUA=FALSE
-DDEFS += -DEFI_HD44780_LCD=FALSE
-
-# This board has no storage
-DDEFS += -DEFI_FILE_LOGGING=FALSE
-USE_FATFS = no
+# we do not have much Lua RAM, let's drop some fancy functions
+DDEFS += -DWITH_LUA_CONSUMPTION=FALSE
+DDEFS += -DWITH_LUA_PID=FALSE
+DDEFS += -DWITH_LUA_STOP_ENGINE=FALSE
--- board_configuration.cpp
+++ board_configuration.cpp
@@ -15,33 +15,12 @@ Gpio getWarningLedPin() {
 // board-specific configuration setup
 void setBoardDefaultConfiguration() {
 
-
 	engineConfiguration->triggerInputPins[0] = Gpio::D3;
 	engineConfiguration->triggerInputPins[1] = Gpio::Unassigned;
 
-
-
-	// engineConfiguration->injectionPins[0] = Gpio::F13;
-	// engineConfiguration->ignitionPins[0] = Gpio::E15;
-	//	engineConfiguration->map.sensor.hwChannel = EFI_ADC_3;
-
-//	engineConfiguration->clt.adcChannel = EFI_ADC_1;
-
-//	engineConfiguration->iat.adcChannel = EFI_ADC_2;
-
-
-    	// 5.6k high side/10k low side = 1.56 ratio divider
     	engineConfiguration->analogInputDividerCoefficient = 1.47f;
-
-    	// 6.34k high side/ 1k low side
-//    	engineConfiguration->vbattDividerCoeff = (6.34 + 1) / 1;
 
 	engineConfiguration->adcVcc = 3.3f;
 
-//	engineConfiguration->clt.config.bias_resistor = 2490;
-//	engineConfiguration->iat.config.bias_resistor = 2490;
 
-
-	// Battery sense on PA0
-//	engineConfiguration->vbattAdcChannel = EFI_ADC_0;
 }
